f98397fd0a229956b6de04680dd1a785902d0c90
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-27 21:19:25 +0900

    イベントハンドラー
      const clickHandler = () => {
        console.log("成功");
      };
    
     <button onClick={clickHandler}>Click</button>

--- src/components/Basic1.js
+++ src/components/Basic1.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const basic1 = (props) => {
+  const clickHandler = () => {
+    console.log("成功");
+  };
   return (
     <>
+      <button onClick={clickHandler}>Click</button>
       <h1>You are {props.name} </h1>
     </>
   )
