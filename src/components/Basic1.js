e6de7ddbd5c9f293f7c2b883c152e39498e9499b
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-28 17:32:49 +0900

    useStateをオブジェクトに対して使う

--- src/components/Basic1.js
+++ src/components/Basic1.js
@@ -1,16 +1,22 @@
 import React, {useState} from 'react'
 
-const Basic1 = (props) => {
-  const clickHandler = () => {
-    console.log("成功");
-  };
+const Basic1 = () => {
 
-  const [count, setCount] = useState(0)
+  const [product, setProducts] = useState({name: "", price: ""})
 
   return (
     <>
-      <button onClick={() => {setCount(prevCount=>prevCount+1); setCount(prevCount=>prevCount+1);}}>Count {count} </button>
-      <h1>You are {props.name} </h1>
+      <form>
+        <input type="text" value={product.name}
+        onChange={evt => setProducts({...product, name: evt.target.value})}/>
+
+        <input type="text" value={product.price}
+        onChange={evt => setProducts({...product, price: evt.target.value})}/>
+
+
+      </form>
+      <h1>Product name is {product.name} </h1>
+      <h1>Product price is {product.price} </h1>
     </>
   )
 }
