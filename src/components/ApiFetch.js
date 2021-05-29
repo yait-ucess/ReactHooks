2310cd4d70c9364830ddfebbbcd550bd536b947b
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-29 17:05:42 +0900

    idを指定して、特定のAPI要素を取得する
    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {method: 'GET'})

--- src/components/ApiFetch.js
+++ src/components/ApiFetch.js
@@ -4,22 +4,28 @@ import axios from 'axios'
 const ApiFetch = () => {
 
   const [posts, setPosts] = useState([]);
+  const [id, setId] = useState(1);
+  const [clicked, setClicked] = useState(false);
+  const handlerClicked = () => {
+    setClicked(!clicked);
+
+  }
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts', {method: 'GET'})
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {method: 'GET'})
     .then(res => res.json())
     .then(data => {
       setPosts(data)
     })
-  }, [])
+  }, [clicked])
 
   return (
     <div>
-      <ul>
-        {
-          posts.map(post => <li key={post.id}> {post.title} </li>)
-        }
-      </ul>
+      <input type='text' value={id} onChange={evt=>setId(evt.target.value)} />
+      <br />
+      <button type='button' onClick={handlerClicked}>Get post</button>
+      <br />
+      {posts.title}
     </div>
   )
 }
