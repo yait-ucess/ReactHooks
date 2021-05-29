09d35ab0a45faff0509bab09dcd53d1a7a3eb5f1
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-29 17:23:10 +0900

    Provider
    <AppContext.Provider value={'value from App.js'}></AppContext.Provider>
    Providerで囲まれた中に埋め込まれているコンポーネントの中で、Providerから提供されるvalueが利用できるようになります。

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 // import UseEffect from './components/UseEffect';
 // import TimerContainer from './components/TimerContainer';
-import ApiFetch from './components/ApiFetch';
+// import ApiFetch from './components/ApiFetch';
+import AppContext from './contexts/AppContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <Basic1 /> */}
-        {/* <Basic2 name="successful man" /> */}
-        {/* <UseEffect /> */}
-        {/* <TimerContainer /> */}
-        <ApiFetch />
-      </header>
-    </div>
+    <AppContext.Provider value={'value from App.js'}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* <Basic1 /> */}
+          {/* <Basic2 name="successful man" /> */}
+          {/* <UseEffect /> */}
+          {/* <TimerContainer /> */}
+          <ApiFetch />
+        </header>
+      </div>
+    </AppContext.Provider>
   );
 }
 
