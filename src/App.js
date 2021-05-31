82231c725b1041d94ce929f21bae58175b9b5c6a
yait-ucess <bandai28@ezweb.ne.jp>
2021-05-31 21:37:37 +0900

    useCallback
    useCallbackはメモ化したコールバック関数を返すHooks APIです。
    
    useCallbackを利用しない場合はコールバック関数はfunctionalコンポーネントの再描画のたびに新しい関数インスタンスを生成してイベントとしてバインドされていきます。
    
    useCallbackを利用することで関数をメモ化して新しい関数インスタンスを生成せずに再描画後のイベントとして再利用を行います。

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import AppContext from './contexts/AppContext';
 // import B from './components/B';
 // import BasicReducer from './components/BasicReducer';
 // import CompB from './components/CompB';
-import Memo from './components/Memo';
+// import Memo from './components/Memo';
 
-import { useReducer } from 'react'
+import { useReducer, useState, useCallback } from 'react';
+import CountDisplay from './components/CountDisplay';
+import CountClick from './components/CountClick';
 
 const initialState = 0;
 const reducer = (currentState, action) => {
@@ -27,6 +29,17 @@ const reducer = (currentState, action) => {
 
 function App() {
   const [count, dispatch] = useReducer(reducer, initialState);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const AddCount1 = useCallback(() => {
+    setCount1(prevCount1 => prevCount1 + 1 )
+  }, [count1])
+
+  const AddCount2 = useCallback(() => {
+    setCount2(prevCount2 => prevCount2 + 1 )
+  }, [count2])
+
   return (
     <AppContext.Provider value={{countProvided: count, dispatchProvided: dispatch}}>
       <div className="App">
@@ -41,7 +54,11 @@ function App() {
           {/* <BasicReducer /> */}
           {/* Count {count}
           <CompB /> */}
-          <Memo />
+          {/* <Memo /> */}
+          <CountDisplay name="count1" count={count1} />
+          <CountClick handleClick={AddCount1}>AddCount1</CountClick>
+          <CountDisplay name="count2" count={count2} />
+          <CountClick handleClick={AddCount2}>AddCount2</CountClick>
         </header>
       </div>
     </AppContext.Provider>
